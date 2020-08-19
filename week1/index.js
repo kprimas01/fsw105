0e365387d1439611f1f582bac9f1fa28e6f98597
kprimas01 <65737910+kprimas01@users.noreply.github.com>
2020-08-19 11:53:51 -0400

    fixing git hub

---
+++ week1/index.js
@@ -0,0 +1,14 @@
+var groceryList = [
+            ["apples",5]
+            ["bacon" ,2]
+            ["beef",4]
+            ["orange juice",2]
+            ["onions",2]
+            ];
+var groceryList = {
+    fruit: "apples",
+    vegtables:"carrots",
+    pork:"bacon",
+    Beef:"groundbeef",
+    }
+
---
+++ week2/index.js
@@ -0,0 +1,25 @@
+var person = {
+    Firstname: "kristin",
+    lastname:"Primas",
+    isafemale: "true"
+    
+
+
+}
+var age = "32"
+
+{
+    var is32= true
+    var is18orolder = true
+}
+var numbers =[ 1,2,3,4]
+numbers[3]
+
+
+
+ 
+
+
+
+
+
