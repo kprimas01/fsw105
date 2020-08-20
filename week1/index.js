fc5e8fb7a585fa0f5a0de3a7248850b7070fa6c5
kprimas01 <65737910+kprimas01@users.noreply.github.com>
2020-08-19 20:58:51 -0400

    grocerylist

--- week1/index.js
+++ week1/index.js
@@ -1,14 +1,16 @@
-var groceryList = [
-            ["apples",5]
-            ["bacon" ,2]
-            ["beef",4]
-            ["orange juice",2]
-            ["onions",2]
-            ];
+
 var groceryList = {
-    fruit: "apples",
+    fruit: "apples", totalamount:15,donecheckout:true,
     vegtables:"carrots",
     pork:"bacon",
     Beef:"groundbeef",
+    groceryList : [
+        ["apples",5],
+        ["bacon" ,2],
+        ["beef",4],
+        ["orange juice",2],
+        ["onions",2],
+        ],
     }
+    console.log(groceryList.groceryList[2])
 
