5270fc16947e1f2748dbbe73dfc2434a41612120
kprimas01 <65737910+kprimas01@users.noreply.github.com>
2020-08-22 21:11:59 -0400

    week2

--- week2/index.js
+++ week2/index.js
@@ -1,20 +1,33 @@
 var person = {
     Firstname: "kristin",
     lastname:"Primas",
-    isafemale: "true"
-    
+    isafemale: "true",
+    age = "32", }
 
-
-}
-var age = "32"
 
 {
     var is32= true
     var is18orolder = true
 }
-var numbers =[ 1,2,3,4]
-numbers[3]
- console.log(true)
+var num1 = 5
+var num2 = 4
+var conditional = num1 >num2
+if ( conditional){
+    console.log("5 is greater then 3")
+}
+var cat = "cat" 
+console.log (3+3)
+if (cat.length ===3)
+{
+    var dog = "dog"
+    console.log (3+3)
+}
+if (dog.length ===3)
+ 
+{
+
+}
+
 
 
 
