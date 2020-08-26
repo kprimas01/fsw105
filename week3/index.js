2cc9b41cccf6675274b0477f0be38e69afd10a69
kprimas01 <65737910+kprimas01@users.noreply.github.com>
2020-08-26 17:07:43 -0400

    project3

---
+++ week3/index.js
@@ -0,0 +1,23 @@
+var name = "Hello"
+toUpperCase & toLowerCase()
+var uppercaseHello =name.toUpperCase()
+console.log(uppercaseHello)
+var lowercasedKristin = uppercasedHello.toLowerCase()
+console.log (lowercasedHello)
+
+var s = "s"
+var newName = name.concat(s)
+console.log (newName)
+
+var animal = "Hello"
+var characterArr = Word.split("l")
+console.log(characterArr)
+
+var phoneNuber = "7176600756"
+var first3 = phoneNuber.slice(0,3)
+var next3 = phoneNumber.slice(3,6)
+var last4 = phoneNuber.slice(6)
+console.log (last4)
+
+var phoneNumberWithDashes = first3 + "-"+ next 3 + next3 + "-" + last4
+console.log (phoneNumberWithDashes)
---
+++ week4/index.js
@@ -0,0 +1,7 @@
+const readline=require("readline-sync")
+
+function add (a,b){
+    return a+b;
+
+}
+add(3,4,5,6,7,8)
